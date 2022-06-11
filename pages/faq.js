5a8403bef2b2e99d697331f1d07eabfdf8072f8f
Daniel H Pavey <danielhpavey@gmail.com>
2022-06-11 16:00:54 +0100

    :recycle:  Update FAQ page

--- pages/faq.js
+++ pages/faq.js
@@ -11,16 +11,13 @@ export default function FaqPage({ posts }) {
   return (
     <div className=''>
       <h1 className="text-5xl mb-7">Frequently Asked Questions</h1>
-      {posts.map(({ slug, frontmatter }) => (
-        <div
-          key={slug}
-          className=''
-        >
-          <Link href={`/faq/${slug}`}>
-            <a>
-              <h1 className=''>{frontmatter.title}</h1>
-            </a>
-          </Link>
+      <img src="/tiny-house-single-bedroom.jpg#right" />
+      {posts.map(({ slug, frontmatter, content }) => (
+        <div key={slug} className=''>
+          <details className='my-4 cursor-pointer'>
+            <summary className="my-4">{frontmatter.title}</summary>
+            <div className="cursor-auto" dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+          </details>
         </div>
       ))}
     </div>
@@ -33,10 +30,11 @@ export async function getStaticProps() {
   const posts = files.map((fileName) => {
     const slug = fileName.replace('.md', '');
     const readFile = fs.readFileSync(`posts/faq/${fileName}`, 'utf-8');
-    const { data: frontmatter } = matter(readFile);
+    const { data: frontmatter, content } = matter(readFile);
     return {
       slug,
       frontmatter,
+      content
     };
   });
 
