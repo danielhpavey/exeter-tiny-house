33e9c128b241c246833d4c22f2dd4bf9f96af60b
Daniel H Pavey <danielhpavey@gmail.com>
2022-08-21 09:44:32 +0100

    🔍️ Fix twitter OG

--- components/TwitterHead.js
+++ components/TwitterHead.js
@@ -7,7 +7,7 @@ const TwitterHead = ({ description, ogUrl, ogImage, ogTitle }) => {
       <meta name="twitter:description" content={description} />
       <meta name="twitter:creator" content="@danielhpavey" />
       <meta property="og:url" content={process.env.NEXT_PUBLIC_URL + ogUrl} />
-      <meta name="twitter:image" content={ogImage} />
+      <meta name="twitter:image" content={process.env.NEXT_PUBLIC_URL + "/" + ogImage} />
     </>
   );
 };
