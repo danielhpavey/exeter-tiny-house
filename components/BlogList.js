75d74f0ee87bbb97f02c09b93dd968d284c4d15b
Daniel <danielhpavey@gmail.com>
2023-02-20 20:33:00 +0000

    Use matter in blog widget

--- components/BlogList.js
+++ components/BlogList.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 const ccase = require("change-case");
+const matter = require('gray-matter');
 
 module.exports = {
     get: (pagePath) => {
         if (pagePath.slice(-1) != "/") pagePath += "/";
         let files = fs.readdirSync(pagePath);
+        files.sort((a, b) => a.position > b.position ? 1 : -1 );
         files = files.filter((file) => {
             if (file == "_app.js") return false;
             const stat = fs.lstatSync(pagePath + file);
@@ -20,9 +22,12 @@ module.exports = {
                 };
             } else {
                 link = path.parse(file).name;
+                stuff = matter(file)
+                const readFile = fs.readFileSync(`posts/blog/${path.parse(file).name}.md`, 'utf-8');
+                const { data: frontmatter, content } = matter(readFile);
                 return {
                     link: 'blog/' + link,
-                    name: ccase.capitalCase(link),
+                    name: frontmatter.metaTitle
                 };
             }
         });
--- components/BlogWidget.js
+++ components/BlogWidget.js
@@ -3,7 +3,7 @@ export default function BlogWidget() {
         <aside className="mb-7">
             <h1 className="text-2xl md:text-4xl mb-3 md:mb-7">Latest Blog Posts</h1>
             <ul>
-            {process.env.blogList.slice(0, 3).map(({link, name}) => (
+            {process.env.blogList.slice(0, 4).map(({link, name}) => (
                 <li key={link} className="mb-2"><a href={link}>{name}</a></li>
             ))}
             </ul>
