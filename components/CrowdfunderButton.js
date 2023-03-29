86ca54c739e90797ce0f90caf75bfa7ad6d396cb
Daniel <danielhpavey@gmail.com>
2023-03-29 18:53:02 +0000

    update crowdfunder component to prevent build fails on netlify

--- components/CrowdfunderButton.js
+++ components/CrowdfunderButton.js
@@ -1,7 +1,7 @@
 const CrowdfunderButton = ({ description, ogUrl, ogImage, ogTitle }) => {
     return (
       <>
-        <div className="crowdfunder-widget" data-project-title="Help launch Exeter Tiny House Community" data-project-id="ethc" data-template-id="4185379753100011" data-theme="default" data-style-height="48px" data-style-width="240px"><a href="https://www.crowdfunder.co.uk/p/ethc" rel="noopener" target="_blank">Help launch Exeter Tiny House Community</a></div>
+        <div className="crowdfunder-widget" data-project-title="Help launch Exeter Tiny House Community" data-project-id="ethc" data-template-id="4185379753100011" data-theme="default" data-style-height="48px" data-style-width="240px"><a href="https://www.crowdfunder.co.uk/p/ethc" rel="noreferrer" target="_blank">Help launch Exeter Tiny House Community</a></div>
       </>
     );
   };
