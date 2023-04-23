9dadd981609522ab0e624cf511380f563e9b2f38
Daniel <danielhpavey@gmail.com>
2023-04-23 14:45:19 +0000

    Tweek profile styles

--- components/Profile.js
+++ components/Profile.js
@@ -4,7 +4,7 @@ export default function Profile(data) {
     return (
         <div className='mb-14'>
             <img src={data.f.image} className="max-w-[200px] mb-7 rounded-full"/>
-            <h2 className="text-3xl text-center">{data.f.title}</h2>
+            <h2 className="text-3xl text-center my-4">{data.f.title}</h2>
             <div dangerouslySetInnerHTML={{ __html: md().render(data.c) }} />
         </div>
     )
